42c19b2ab9387127658e202c0daad5a599f4b3fb
stdev321 <st.dev321@gmail.com>
2023-01-25 16:37:34 +0530

    initial-commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const {readFile} = require('fs')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  readFile('./home.html', 'utf-8',(err, html) =>{
+    if(err){
+        res.send("Service unavailable")
+    }
+    res.send(html)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
